728f95233bb138d6a3c50c5f26fbe8919a9f6762
Vishal T <vishal@Vishals-MacBook-Air.local>
2025-04-27 15:32:17 +0530

    fk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
         <div className="flex items-center mb-4 md:mb-0">
           <div className="bg-white p-1 rounded-md shadow-md">
             {/* === College Logo inserted properly (rectangular) === */}
-            <img src="/SRMcolor.png" alt="SRM College Logo" className="h-16 w-auto object-contain" />
+            <img src="https://cdn.discordapp.com/attachments/1353249878779756566/1365990822691999744/SRMcolor-min_1.png?ex=680f5197&is=680e0017&hm=5cf4794db6d758cadbbeba23e48e10f646c812a82d2924f1a4ee7ed1cb3670e2&" alt="SRM College Logo" className="h-16 w-auto object-contain" />
             {/* ================================================ */}
           </div>
           <div className="ml-4">
